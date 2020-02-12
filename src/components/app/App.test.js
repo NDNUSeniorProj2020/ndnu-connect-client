1cbe62e2bc9d2551fc772420d6958462656dda8b
Christian Lapinig <clapinig93@gmail.com>
2020-02-11 19:45:05 -0800

    Remove create-react-app boilerplate

--- src/components/app/App.test.js
+++ src/components/app/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders NDNU Connect header', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const h1Element = getByText(/NDNU Connect/i);
+  expect(h1Element).toBeInTheDocument();
 });
