abb6577abcf7a2116a99be0e968c313d0c8726ca
jorgesv16 <jorge@github.com>
2020-02-18 12:40:34 -0800

    Added styling and dropdown to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,8 +2,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
-import { Menu, Icon } from 'antd';
+import { Avatar, Dropdown, Icon, Menu } from 'antd';
 import logo from '../../assets/ndnu-logo.jpg';
+import './Navbar.css';
 
 const { SubMenu } = Menu;
 
@@ -13,19 +14,31 @@ class Navbar extends React.Component {
   };
 
   handleClick = e => {
-    console.log('click ', e);
     this.setState({
       current: e.key,
     });
   };
 
   render() {
+    const avatarMenu = (
+      <Menu>
+        <Menu.Item key="0" style={{padding:'8px 35px 8px 35px'}}><Icon type="setting" theme="filled" />Settings</Menu.Item>
+        <Menu.Divider />
+        <Menu.Item key="1" style={{padding:'8px 35px 8px 35px'}}><Icon type="logout" />Logout</Menu.Item>
+      </Menu>
+    );
+
     return (
       <div>
-        <img  src={logo} style={{height:'60px', float:'left'}} alt="NDNU Logo"/>
-        <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
+        <img  src={logo} className="ndnu-logo" alt="NDNU Logo" />
+
+        <Dropdown overlay={avatarMenu} trigger={['click']} className="avatar-dropdown-container">
+          <Avatar size="large" className="avatar">J</Avatar>
+        </Dropdown>
+
+        <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal" className="navbar-container">
           <SubMenu
-            style={{marginLeft:'100px'}}
+            className="tutor-menu-item"
             title={
               <span className="submenu-title-wrapper">
                 <Icon type="book" />
