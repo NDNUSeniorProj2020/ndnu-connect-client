f6b5caa4356f83712b59e85e3d29252ce74b9061
Christian Lapinig <clapinig93@gmail.com>
2020-02-21 20:45:12 -0800

    Test handleLogout and handleLogin functions in App

--- src/components/app/__tests__/App.test.js
+++ src/components/app/__tests__/App.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
 
 import App from '../App';
+
+configure({ adapter: new Adapter() });
 
 /*
 * Don't worry about warning signs in our editor/IDE
@@ -12,6 +16,22 @@ describe('Testing App component', () => {
 		it('should render without crashing', () => {
 			const component = renderer.create(<App />);
 			expect(component).toMatchSnapshot();
+		});
+	});
+
+	describe('Integration tests', () => {
+		it('should change loggedIn and user state correctly when user logs in and logs out', () => {
+			const component = shallow(<App />);
+			const instance = component.instance();
+
+			instance.handleLogin({ token: 'token', user: { username: 'user' } });
+			expect(component.state('loggedIn')).toEqual(true);
+			expect(component.state('user')).toEqual({ username: 'user' });
+
+			instance.handleLogout();
+			expect(component.state('loggedIn')).toEqual(false);
+			expect(component.state('user')).toEqual({});
+
 		});
 	});
 });
