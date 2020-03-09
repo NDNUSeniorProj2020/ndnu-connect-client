56d7c20355e775719fac63b8fce0de4340e435ea
Christian Lapinig <clapinig93@gmail.com>
2020-03-09 00:20:35 -0700

    Add store to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './components/app/App';
+import reducers from './reducers/index';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, applyMiddleware(thunk));
+const app = (
+	<Provider store={store}>
+		<App />
+	</Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
