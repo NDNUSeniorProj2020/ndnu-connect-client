08156d0ad073c961cad5c0573c0345a086499d94
Christian Lapinig <clapinig93@gmail.com>
2020-03-09 00:13:18 -0700

    Add unit tests for Home component

--- src/components/Home/__tests__/Home.test.js
+++ src/components/Home/__tests__/Home.test.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
+import React from "react";
+import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
 
-import Home from '../Home';
+import Home from "../Home";
+import TwitterFeed from "../TwitterFeed";
 
-describe('Testing Home component', () => {
-	it('should render without crashing.', () => {
-		const tree = renderer.create(<Home />);
-		expect(tree).toMatchSnapshot();
+describe('Home component tests', () => {
+	describe('snapshot tests', () => {
+		it('should render without crashing.', () => {
+			const tree = renderer.create(<Home />);
+			expect(tree).toMatchSnapshot();
+		});
+	});
+
+	describe('unit tests', () => {
+		const wrapper = shallow(<Home />);
+		expect(wrapper.contains(<TwitterFeed />)).toBe(true);
 	});
 });
