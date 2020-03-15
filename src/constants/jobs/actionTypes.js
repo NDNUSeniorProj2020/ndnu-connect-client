0d5c722db739ff8f8561e82ff00e6a2050793d4b
Christian Lapinig <clapinig93@gmail.com>
2020-03-14 17:44:10 -0700

    Add reducers with tests for fetching all jobs

---
+++ src/constants/jobs/actionTypes.js
@@ -0,0 +1,4 @@
+// Fetch all jobs constants
+export const FETCH_JOBS_REQUEST = 'FETCH_JOBS_REQUEST';
+export const FETCH_JOBS_SUCCESS = 'FETCH_JOBS_SUCCESS';
+export const FETCH_JOBS_FAILURE = 'FETCH_JOBS_FAILURE';
---
+++ src/reducers/jobs/jobsReducers.test.js
@@ -0,0 +1,33 @@
+import {
+	FETCH_JOBS_SUCCESS
+} from "../../constants/jobs/actionTypes";
+import jobsReducers from './jobsReducers';
+
+const jobs = [
+	{
+		"id": 1,
+		"title": "test1",
+		"description": "description test",
+		"qualifications": "bs degree",
+		"pay": "1234",
+		"link": "google.com",
+		"date": "2020-03-13T11:55:20.710240-07:00",
+		"type": "FULL",
+		"person": 1
+	}
+];
+
+describe('tests for jobsReducers', () => {
+	const beforeState = { jobs: [], success: false };
+
+	it('should return initial state', () => expect(jobsReducers(undefined, {})).toEqual({ jobs: [], success: false }));
+
+	describe('tests for fetch all jobs reducers', () => {
+		it('sets up jobs', () => {
+			const action = { type: FETCH_JOBS_SUCCESS, payload: { jobs } };
+			const afterState = jobsReducers(beforeState, action);
+
+			expect(afterState).toEqual({ jobs, success: true });
+		});
+	});
+});
