5c8b3d1c6817cd3303a05fd4e562e8fcdd8d3396
Christian Lapinig <clapinig93@gmail.com>
2020-03-15 12:59:39 -0700

    Add studentReducer to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import authReducer from "./auth/authReducers";
 import departmentReducers from "./department/departmentReducer";
 import jobsReducers from "./jobs/jobsReducer";
+import studentReducer from "./student/studentReducer";
 
 export default combineReducers({
 	authReducer,
 	departmentReducers,
-	jobsReducers
+	jobsReducers,
+	studentReducer
 });
