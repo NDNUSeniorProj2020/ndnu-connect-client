f107f07aeafea2ee4acffa602484d63e1f2c1501
Christian Lapinig <clapinig93@gmail.com>
2020-03-24 20:35:26 -0700

    Use condensed constants in authenticationActions

--- src/actions/auth/authenticationActions.js
+++ src/actions/auth/authenticationActions.js
@@ -1,40 +1,32 @@
 import api from "../../api";
 import {
-	LOGIN_USER_REQUEST,
-	LOGIN_USER_SUCCESS,
-	LOGIN_USER_FAILURE,
-	USER_REGISTRATION_REQUEST,
-	USER_REGISTRATION_SUCCESS,
-	USER_REGISTRATION_FAILURE,
-	LOGOUT_USER_REQUEST,
-	LOGOUT_USER_SUCCESS,
-	HAS_TOKEN_REQUEST,
+	LOGIN_SUCCESS,
+	LOGIN_FAILURE,
+	REGISTRATION_SUCCESS,
+	REGISTRATION_FAILURE,
+	LOGOUT_SUCCESS,
+	LOGOUT_FAILURE,
 	HAS_TOKEN_SUCCESS,
 	HAS_TOKEN_FAILURE
-} from "../../constants/auth/actionTypes";
+} from "../../constants/actionTypes";
 
 // Login actions
-export function loginRequest() {
-	return { type: LOGIN_USER_REQUEST };
-}
-
 export function loginSuccess(data) {
 	return {
-		type: LOGIN_USER_SUCCESS,
+		type: LOGIN_SUCCESS,
 		payload: { user: { ...data.user } }
 	};
 }
 
 export function loginFailure(errors) {
 	return {
-		type: LOGIN_USER_FAILURE,
-		errors
+		type: LOGIN_FAILURE,
+		payload: { errors }
 	};
 }
 
 export function login(user) {
 	return (dispatch) => {
-		dispatch(loginRequest());
 		return api().post('/accounts/login/', user)
 			.then(res => res.data)
 			.then(data => dispatch(loginSuccess(data)))
@@ -43,29 +35,24 @@ export function login(user) {
 }
 
 // User registration actions
-export function registrationRequest() {
-	return { type: USER_REGISTRATION_REQUEST }
-}
-
 export function registrationSuccess(data) {
 	const { user } = data;
 	const { email, token } = user;
 	return {
-		type: USER_REGISTRATION_SUCCESS,
+		type: REGISTRATION_SUCCESS,
 		payload: { user: { email, token } }
 	};
 }
 
 export function registrationFailure(errors) {
 	return {
-		type: USER_REGISTRATION_FAILURE,
-		errors
+		type: REGISTRATION_FAILURE,
+		payload: { errors }
 	};
 }
 
 export function register(user) {
 	return (dispatch) => {
-		dispatch(registrationRequest());
 		return api().post('/accounts/register/', user)
 			.then(res => res.data)
 			.then(data => dispatch(registrationSuccess(data)))
@@ -74,29 +61,20 @@ export function register(user) {
 }
 
 // Logout actions
-export function logoutRequest() {
-	return { type: LOGOUT_USER_REQUEST };
-}
-
 export function logoutSuccess() {
 	return {
-		type: LOGOUT_USER_SUCCESS,
+		type: LOGOUT_SUCCESS,
 		payload: { user: {} }
 	};
 }
 
 export function logout() {
 	return (dispatch) => {
-		dispatch(logoutRequest());
 		dispatch(logoutSuccess());
-	}
+	};
 }
 
 // Has token actions
-export function hasTokenRequest() {
-	return { type: HAS_TOKEN_REQUEST };
-}
-
 export function hasTokenSuccess(data) {
 	const { user } = data;
 	const { email, token } = user;
@@ -115,11 +93,7 @@ export function hasTokenFailure(err) {
 
 export function hasToken(token) {
 	return (dispatch) => {
-		const headers = {
-			Authorization: `Token ${token}`
-		};
-
-		dispatch(hasTokenRequest());
+		const headers = { Authorization: `Token ${token}` };
 		return api().get('/accounts/user/', { headers })
 			.then(res => res.data)
 			.then(data => dispatch(hasTokenSuccess(data)))
