b7907949317c1b27749512ba5152447d0937feb7
Christian Lapinig <clapinig93@gmail.com>
2020-04-01 18:20:36 -0700

    Remove Jobs import in Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Jobs from '../Jobs';
+
 import './Home.css';
 import TwitterFeed from './TwitterFeed';
 import Tutors from './Tutors';
