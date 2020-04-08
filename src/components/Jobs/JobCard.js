487bed126032d723d09c7e30959253de3e63b018
Dbolarinwa <damibo2@gmail.com>
2020-04-07 21:34:40 -0700

    add job card content

--- src/components/Jobs/JobCard.js
+++ src/components/Jobs/JobCard.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card } from 'antd';
+import moment from 'moment';
 
 export default function JobCard({ job }) {
 	return (
 		<div>
-			<p>Job card</p>
+			<Card title={job.title} extra={<a href={job.link}>Apply</a>}>
+			<p>{job.description}</p>
+			<p>{moment(job.date).format('LLLL')}</p>
+
+			</Card>
+			
 		</div>
 	);
 }
