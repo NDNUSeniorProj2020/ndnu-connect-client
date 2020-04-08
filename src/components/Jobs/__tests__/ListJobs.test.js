d84fc2c5f58638d89549f68dce146d6e342e6b54
Christian Lapinig <clapinig93@gmail.com>
2020-04-08 15:49:43 -0700

    Fix ListJobs snapshots to pass CI

--- src/components/Jobs/__tests__/ListJobs.test.js
+++ src/components/Jobs/__tests__/ListJobs.test.js
@@ -7,12 +7,12 @@ describe('tests for ListJobs', () => {
 	describe('snapshot tests', () => {
 		it('renders without crashing without props', () => {
 			const tree = shallow(<ListJobs />);
-			expect(tree.html()).toMatchSnapshot();
+			expect(tree).toMatchSnapshot();
 		});
 
 		it('renders without crashing when props are passed', () => {
 			const tree = shallow(<ListJobs jobs={[{ id: 1, title: 'Bus Driver' }]} />);
-			expect(tree.html()).toMatchSnapshot();
+			expect(tree).toMatchSnapshot();
 		});
 	});
 
