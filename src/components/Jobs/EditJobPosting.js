f5d64b0debba12863cf0d0d745d0edf1523f42e0
Dbolarinwa <damibo2@gmail.com>
2020-04-19 11:27:09 -0700

    created edit job component

--- src/components/Jobs/EditJobPosting.js
+++ src/components/Jobs/EditJobPosting.js
@@ -2,7 +2,30 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import MarkdownIt from 'markdown-it';
-import MarkdownEditor from 'react-markdown-editor-lite';
-import 'react-markdown-editor-lite/lib/index.css';
+import api from '../../api';
+import createAuthHeader from '../../assets/js/createAuthHeader';
+import JobListingForm from './JobListingForm';
 
+export function ConnectedEditJobListing({ history, success, updateJob }) {
+    const editJob = async (job) => {
+      const headers = createAuthHeader(localStorage.getItem('token'));
+  
+      try {
+        const res = await api().get('/accounts/current_user/', { headers });
+        const { id } = res.data;
+        updateJob(localStorage.getItem('token'), job, id);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+  
+      return (
+          <div>
+        <header>
+          <h1>Edit Job Listing</h1>
+        </header>
+              <JobListingForm submitJob={editJob} />
+          </div>
+      );
+  }
+  
