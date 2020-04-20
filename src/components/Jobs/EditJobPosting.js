41683e698654b95ca072d6e0c4ac827825560ba4
Christian Lapinig <clapinig93@gmail.com>
2020-04-19 18:46:46 -0700

    Formatting work for EditJobPosting

--- src/components/Jobs/EditJobPosting.js
+++ src/components/Jobs/EditJobPosting.js
@@ -7,25 +7,25 @@ import createAuthHeader from '../../assets/js/createAuthHeader';
 import JobListingForm from './JobListingForm';
 
 export function ConnectedEditJobListing({ history, success, updateJob }) {
-    const editJob = async (job) => {
-      const headers = createAuthHeader(localStorage.getItem('token'));
-  
-      try {
-        const res = await api().get('/accounts/current_user/', { headers });
-        const { id } = res.data;
-        updateJob(localStorage.getItem('token'), job, id);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-  
-      return (
-          <div>
-        <header>
-          <h1>Edit Job Listing</h1>
-        </header>
-              <JobListingForm submitJob={editJob} />
-          </div>
-      );
-  }
-  
+  const editJob = async (job) => {
+    const headers = createAuthHeader(localStorage.getItem('token'));
+
+    try {
+      const res = await api().get('/accounts/current_user/', { headers });
+      const { id } = res.data;
+      updateJob(localStorage.getItem('token'), job, id);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <header>
+        <h1>Edit Job Listing</h1>
+      </header>
+      <JobListingForm submitJob={editJob} />
+    </div>
+  );
+}
+
