bb8af2758a67ab02cdcb7ea44534822d3eccf562
Christian Lapinig <clapinig93@gmail.com>
2020-04-21 00:17:06 -0700

    Add actions and reducers for updating and fetching a single job

--- src/actions/jobs/jobsActions.js
+++ src/actions/jobs/jobsActions.js
@@ -2,8 +2,12 @@ import api from '../../api';
 import {
 	FETCH_ALL_JOBS_SUCCESS,
   FETCH_ALL_JOBS_FAILURE,
+  FETCH_SINGLE_JOB_SUCCESS,
+  FETCH_SINGLE_JOB_FAILURE,
   SAVE_JOB_SUCCESS,
-  SAVE_JOB_FAILURE
+  SAVE_JOB_FAILURE,
+  UPDATE_JOB_SUCCESS,
+  UPDATE_JOB_FAILURE
 } from '../../constants/jobs/actionTypes';
 import createAuthHeader from '../../assets/js/createAuthHeader';
 
@@ -33,6 +37,32 @@ export function fetchJobs(token) {
 	};
 }
 
+// Fetch single job actions
+export function fetchJobSuccess(data) {
+  return {
+    type: FETCH_SINGLE_JOB_SUCCESS,
+    payload: { job: { ...data } }
+  };
+}
+
+export function fetchJobFailure(errors) {
+  return {
+    type: FETCH_SINGLE_JOB_FAILURE,
+    payload: { errors }
+  };
+}
+
+export function fetchJob(token, jobId) {
+  return (dispatch) => {
+    const headers = createAuthHeader(token);
+
+    return api().get(`/api/job/${jobId}/retrieve/`, { headers })
+      .then(res => res.data)
+      .then(data => dispatch(fetchJobSuccess(data)))
+      .catch(errors => dispatch(fetchJobFailure(errors.response.data)));
+  };
+}
+
 // Create job listing actions
 export function createJobSuccess() {
   return { type: SAVE_JOB_SUCCESS };
@@ -52,5 +82,27 @@ export function createJob(token, job, userId) {
     return api().post('/api/job/', { ...job, user: userId }, { headers })
       .then(() => dispatch(createJobSuccess()))
       .catch(errors => dispatch(createJobFailure(errors.response.data)));
+  };
+}
+
+// Update job listing actions
+export function updateJobSuccess() {
+  return { type: UPDATE_JOB_SUCCESS };
+}
+
+export function updateJobFailure(errors) {
+  return {
+    type: UPDATE_JOB_FAILURE,
+    payload: { errors }
+  };
+}
+
+export function updateJob(token, job) {
+  return (dispatch) => {
+    const headers = createAuthHeader(token);
+
+    return api().put(`/api/job/${job.id}/update/`, { ...job }, { headers })
+      .then(() => dispatch(updateJobSuccess()))
+      .catch(errors => dispatch(updateJobFailure(errors.response.data)));
   };
 }
--- src/actions/jobs/jobsActions.test.js
+++ src/actions/jobs/jobsActions.test.js
@@ -5,10 +5,14 @@ import configureMockStore from 'redux-mock-store';
 import {
 	FETCH_ALL_JOBS_SUCCESS,
   FETCH_ALL_JOBS_FAILURE,
+  FETCH_SINGLE_JOB_SUCCESS,
+  FETCH_SINGLE_JOB_FAILURE,
   SAVE_JOB_SUCCESS,
-  SAVE_JOB_FAILURE
+  SAVE_JOB_FAILURE,
+  UPDATE_JOB_SUCCESS,
+  UPDATE_JOB_FAILURE
 } from '../../constants/jobs/actionTypes';
-import { fetchJobs, createJob } from './jobsActions';
+import { fetchJobs, fetchJob, createJob, updateJob } from './jobsActions';
 
 const jobs = [
 	{
@@ -20,9 +24,10 @@ const jobs = [
 		'link': 'google.com',
 		'date': '2020-03-13T11:55:20.710240-07:00',
 		'type': 'FULL',
-		'person': 1
+		'user': 1
 	}
 ];
+const job = jobs[0];
 const newJob = {
   'title': 'test1',
   'description': 'description test',
@@ -73,6 +78,30 @@ describe('tests for job actions', () => {
 		});
   });
 
+  describe('testing actions for fetching single job', () => {
+    it('fetches job successfully', async () => {
+      httpMock.onGet(`${url}/api/job/${job.id}/retrieve/`).reply(200, job);
+
+      fetchJob('someRandomToken', job.id)(store.dispatch);
+      await flushAllPromises();
+
+      expect(store.getActions()).toEqual([
+        { type: FETCH_SINGLE_JOB_SUCCESS, payload: { job } }
+      ]);
+    });
+
+    it('throws errors when job cannot be fetched', async () => {
+      httpMock.onGet(`${url}/api/job/${job.id}/retrieve/`).reply(500, errors);
+
+      fetchJob('someRandomToken', job.id)(store.dispatch);
+      await flushAllPromises();
+
+      expect(store.getActions()).toEqual([
+        { type: FETCH_SINGLE_JOB_FAILURE, payload: { errors: { ...errors } } }
+      ]);
+    });
+  });
+
   describe('testing actions for saving job postings', () => {
     it('creates job posting successfully', async () => {
       httpMock.onPost(`${url}/api/job/`).reply(200, { success: true });
@@ -93,6 +122,30 @@ describe('tests for job actions', () => {
 
       expect(store.getActions()).toEqual([
         { type: SAVE_JOB_FAILURE, payload: { errors: { ...errors } } }
+      ]);
+    });
+  });
+
+  describe('testing actions for updating job postings', () => {
+    it('updates job posting', async () => {
+      httpMock.onPut(`${url}/api/job/${job.id}/update/`).reply(200, { success: true });
+
+      updateJob('randomtoken', job)(store.dispatch);
+      await flushAllPromises();
+
+      expect(store.getActions()).toEqual([
+        { type: UPDATE_JOB_SUCCESS }
+      ]);
+    });
+
+    it('throws error if job cannot be updated', async () => {
+      httpMock.onPut(`${url}/api/job/${job.id}/update/`).reply(500, errors);
+
+      updateJob('randomtoken', job)(store.dispatch);
+      await flushAllPromises();
+
+      expect(store.getActions()).toEqual([
+        { type: UPDATE_JOB_FAILURE, payload: { errors: { ...errors} } }
       ]);
     });
   });
