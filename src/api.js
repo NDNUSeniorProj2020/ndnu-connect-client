3c631bb50af5546d5232e635c2017c4f2b0bb633
digitaluniverse <contact@mattdaniele.com>
2020-04-21 17:56:23 -0700

    Changed the backend url to https

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default () => axios.create({
-  baseURL: process.env.REACT_APP_API || 'http://ec2-54-241-187-187.us-west-1.compute.amazonaws.com:81'
+  baseURL: process.env.REACT_APP_API || 'https://ec2-54-241-187-187.us-west-1.compute.amazonaws.com:81'
 });
