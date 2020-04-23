40e00a478579a62ef7c85286a5eb054412eac1a4
Christian Lapinig <clapinig93@gmail.com>
2020-04-22 17:23:16 -0700

    Use correct production api url

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
-export const url = process.env.REACT_APP_API || 'https://ec2-54-241-187-187.us-west-1.compute.amazonaws.com:81'
+export const url = process.env.REACT_APP_API || 'https://ec2-54-241-187-187.us-west-1.compute.amazonaws.com'
 export default () => axios.create({ baseURL: url });
