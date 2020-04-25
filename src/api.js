2510f10e0ccf041ed9344dfa91767ddfe7afd526
Christian Lapinig <clapinig93@gmail.com>
2020-04-24 20:56:16 -0700

    Change API url

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
-export const url = process.env.REACT_APP_API || 'https://ec2-54-241-187-187.us-west-1.compute.amazonaws.com'
+export const url = process.env.REACT_APP_API || 'https://praveenv.org'
 export default () => axios.create({ baseURL: url });
