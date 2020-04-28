afe84d47177278564eb2a2e5dbb43bdc47a60233
leoskips34 <leo.samuelson@gmail.com>
2020-04-27 20:24:47 -0700

    HomePages

---
+++ src/components/Login/LoginNavBar.js
@@ -0,0 +1,90 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import { Avatar, Dropdown, Icon, Menu } from 'antd';
+import logo from '../../assets/ndnu-logo.jpg';
+import './LoginNavbar.css';
+
+const { SubMenu } = Menu;
+
+class LoginNavbar extends React.Component {
+  state = {
+    current: 'mail',
+  };
+
+  handleClick = e => {
+    this.setState({
+      current: e.key,
+    });
+  };
+
+  render() {
+    const avatarMenu = (
+      <Menu>
+        <Menu.Item key="0" className="dropdown-spacing"><Icon type="setting" theme="filled" />Settings</Menu.Item>
+        <Menu.Divider />
+        <Menu.Item key="1" className="dropdown-spacing"><Icon type="logout" />Logout</Menu.Item>
+      </Menu>
+    );
+
+    return (
+      <div>
+        <a href="https://www.ndnu.edu"><img  src={logo} className="ndnu-logo" alt="NDNU Logo" /></a>
+
+        <Dropdown overlay={avatarMenu} trigger={['click']} className="avatar-dropdown-container">
+          <Avatar size="large" className="avatar">J</Avatar>
+        </Dropdown>
+        
+        <form>
+        <h1>Hello</h1>
+        <p>Enter your name:</p>
+        <input
+          type="text"
+        />
+        </form>
+
+        <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal" className="navbar-container">
+          <SubMenu
+            className="tutor-menu-item"
+            title={
+              <span className="submenu-title-wrapper">
+                <Icon type="book" />
+                Tutor
+              </span>
+            }
+          >
+            <Menu.Item key="setting:1">Student</Menu.Item>
+            <Menu.Item key="setting:2">Tutor</Menu.Item>
+          </SubMenu>
+          <SubMenu
+            title={
+              <span className="submenu-title-wrapper">
+                <Icon type="shopping" />
+                Jobs | Internships
+              </span>
+            }
+          >
+            <Menu.ItemGroup title="Item 1">
+              <Menu.Item key="setting:1">Option 1</Menu.Item>
+              <Menu.Item key="setting:2">Option 2</Menu.Item>
+            </Menu.ItemGroup>
+            <Menu.ItemGroup title="Item 2">
+              <Menu.Item key="setting:3">Option 3</Menu.Item>
+              <Menu.Item key="setting:4">Option 4</Menu.Item>
+            </Menu.ItemGroup>
+          </SubMenu>
+          <Menu.Item key="alumni">
+            Alumni
+          </Menu.Item>
+          <Menu.Item key="forum">
+            Open Forum
+          </Menu.Item>
+        </Menu>
+      </div>
+    );
+  }
+}
+
+export default LoginNavbar;
+          
